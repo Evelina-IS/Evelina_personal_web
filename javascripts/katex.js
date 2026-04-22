e3562e317894a60f4e73a84755b9efc447732f3a
Evelina <2754438440@qq.com>
2026-04-22 16:04:35 +0800

    Deployed 6d6b96d with MkDocs version: 1.6.1

--- javascripts/katex.js
+++ javascripts/katex.js
@@ -3,9 +3,13 @@ document$.subscribe(({ body }) => {
     delimiters: [
       {left: '$$', right: '$$', display: true},
       {left: '$', right: '$', display: false},
-      {left: '\\(', right: '\\)', display: false},
-      {left: '\\[', right: '\\]', display: true}
+      {left: '\\[', right: '\\]', display: true},
+      {left: '\\(', right: '\\)', display: false}
     ],
+    // 关键配置：忽略特定类名的元素
+    ignoredTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code'],
+    // 允许在代码块外处理 $$
+    ignoredClasses: ['highlight', 'codehilite'],
     throwOnError: false
   });
 });
